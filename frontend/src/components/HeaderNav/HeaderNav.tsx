781f3de8bc40a4ab92687f7284a0d5d0ebe3a420
이두현 <ldh9411@hanmail.net>
2023-09-18 14:20:38 +0900

    Style: HeaderNav Css 수정
    HeaderNav css 작업 진행 중

--- frontend/src/components/HeaderNav/HeaderNav.tsx
+++ frontend/src/components/HeaderNav/HeaderNav.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../recoil/user/user';
 import ButtonComponent from '../common/ButtonComponent';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { LightBulbIcon, ClipboardIcon } from '@heroicons/react/24/outline';
+import {
+  HeartIcon,
+  MusicalNoteIcon,
+  UserIcon,
+} from '@heroicons/react/24/solid';
+import InputComponent from '../common/InputComponent';
 
 export default function HeaderNav() {
   const user = useRecoilValue(userState);
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+  useEffect(() => {
+    handleSearch();
+  }, [search]);
+
+  const onChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSearch = () => {
+    //search 값에 따라 연관된 검색어 밑에 보여주기
+    console.log('검색어:', search);
+  };
+
+  const onHandleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  const navigateToBoard = () => {
+    navigate('/board'); // '/board'로 이동합니다.
+  };
 
   return (
     <div className="fixed h-full w-64 bg-black">
-      <div className="text-2xl px-8">YESRAE</div>
+      <div className="text-4xl px-8 pt-6 pb-4 font-extrabold">YESRAE</div>
       {user.name ? (
         <>
           <div>
@@ -26,12 +57,36 @@ export default function HeaderNav() {
           <ButtonComponent type="ismiddle">로그인</ButtonComponent>
         </Link>
       )}
-      <ul className="text-white px-8 pt-4">
-        <li className="text-xl pt-1">게시판</li>
-        <li className="text-xl pt-1">플레이리스트</li>
-        <li className="text-xl pt-1">이상형월드컵</li>
-        <li className="text-xl pt-1">데일리퀴즈</li>
-        <li className="text-xl pt-1">마이페이지</li>
+      <InputComponent
+        placeholder="YESRAE 검색"
+        onChange={onChangeSearch}
+        onKeyDown={onHandleKeyDown}
+        type="text"
+      />
+      <ul className="text-white px-8">
+        <li
+          className="text-xl pt-1 flex items-center hover:text-red-200"
+          onClick={navigateToBoard}
+        >
+          <ClipboardIcon className="w-8 h-8" />
+          <div className="pl-2">게시판</div>
+        </li>
+        <li className="text-xl pt-3 flex items-center hover:text-red-200">
+          <MusicalNoteIcon className="w-8 h-8" />
+          <div className="pl-2"> 플레이리스트 </div>
+        </li>
+        <li className="text-xl pt-3 flex items-center hover:text-red-200">
+          <HeartIcon className="w-8 h-8" />
+          <div className="pl-2">이상형월드컵</div>
+        </li>
+        <li className="text-xl pt-3 flex items-center hover:text-red-200">
+          <LightBulbIcon className="w-8 h-8" />
+          <div className="pl-2">데일리퀴즈</div>
+        </li>
+        <li className="text-xl pt-3 flex items-center hover:text-red-200">
+          <UserIcon className="w-8 h-8" />
+          <div className="pl-2">마이페이지</div>
+        </li>
       </ul>
     </div>
   );
--- frontend/src/components/common/ButtonComponent.tsx
+++ frontend/src/components/common/ButtonComponent.tsx
@@ -12,32 +12,28 @@ export default function ButtonComponent({
   children,
 }: IButtonProps) {
   let buttonCss: string;
-  let textCss: string;
   let margin = '';
 
   if (type === 'isBig') {
     buttonCss =
       'rounded-xl w-96 h-8 bg-gradient-to-r from-yesrae-0 to-yesrae-100';
-    textCss = 'font-kdisplay text-xl';
     margin = 'm-1';
   } else if (type === 'istiny') {
     buttonCss =
       'rounded w-28 h-8 border-2 bg-gradient-to-r from-yesrae-0 to-yesrae-100 text-xl';
-    textCss = 'font-kdisplay';
   } else if (type === 'ismiddle') {
     buttonCss =
       'rounded w-48 h-10 bg-gradient-to-r from-yesrae-0 to-yesrae-100 text-xl';
-    textCss = 'font-kdisplay';
+    margin = 'border-b border-t py-3 mx-4 border-gray-900';
   } else {
     buttonCss =
       'rounded-xl w-64 h-8 bg-gradient-to-r from-yesrae-0 to-yesrae-100 text-2xl';
-    textCss = 'font-kdisplay text-lg';
   }
 
   return (
     <div className={`flex justify-center ${margin}`}>
       <button type="button" className={buttonCss} onClick={onClick}>
-        <div className={textCss}>{children}</div>
+        <div>{children}</div>
       </button>
     </div>
   );
